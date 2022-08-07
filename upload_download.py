dc219d5bb6f29cf3901a4308aa1b46f14c24d7e4
leonardovenan <leonardovenancio12@gmail.com>
2022-08-07 16:15:16 -0300

    instruções de upload e download de arquivos em um bucket AWS

--- upload_download.py
+++ upload_download.py
@@ -1,3 +1,4 @@
+#primeiro -> upload
 # https://docs.aws.amazon.com/pythonsdk/?id=docs_geteway
 import boto3
 from sagemaker.s3 import S3Uploader
@@ -18,3 +19,16 @@ subpasta = "arquivos"
 #padrão de envio é "s3://{nome do bucket}/{nome da pasta}"
 caminho = "s3://{}/{}".format{bucket, subpasta}
 #print(caminho)
+
+#identificando o arquivo
+s3_upload = S3Uploader.upload("nome do arquivo com extensão.exe")
+
+#Segundo -> download
+import boto3
+
+bucket = "nome-do-bucket"
+s3 = boto3.resource("s3")
+
+#parâmetro de local/nome do arquivo e novo nome do arquivo
+s3.Bucket(bucket).download_file("arquivos/digit.png", "digit_download.png")
+
