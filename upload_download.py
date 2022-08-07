f66fff1b8f3b3c84282ec4496656c9ca8f044007
leonardovenan <leonardovenancio12@gmail.com>
2022-08-07 15:39:15 -0300

    Novo upload no github

--- upload_download.py
+++ upload_download.py
@@ -1,3 +1,7 @@
-print("hello world!")
-#produzindo
-print("hello world 2!")
+# https://docs.aws.amazon.com/pythonsdk/?id=docs_geteway
+import boto3
+from sagemaker.s3 import S3Uploader
+
+session = boto3.Session()
+
+session.region_name
